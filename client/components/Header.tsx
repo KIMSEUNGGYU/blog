7e144b9890fbd02552e0608f91e411e095b5f34e
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-04-27 15:25:46 +0900

    [test] 임시용 테스트 코드 작성

---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+
+export default function Header() {
+    return <Wrapper>
+        Header
+    </Wrapper>
+}
+
+const Wrapper = styled.div(() => ({
+    backgroundColor: 'red'
+}))
