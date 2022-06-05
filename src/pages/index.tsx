d3364a3ea8cd897ea3642c05b11e2cfaea8e923a
kimseunggyu <dev.seunggyu@gmail.com>
2022-06-06 00:53:43 +0900

    [refactor] src 폴더 구조 변경 - apis, styles 폴더 구조 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { Tag, Post, MultiSelectType } from '@/types/index';
-
-import { getDatabaseForTag, getDatabaseForPosts } from '@/apis/index';
-
-import { HOME_POSTS_DATABASE_ID } from 'src/constant';
+import { Tag, Post } from '@/types/index';
 
 import HomePage from '@/page-components/home';
+
+import request from 'src/api';
 
 type Props = {
   tags: Tag[];
@@ -21,22 +19,7 @@ export default function Index({ tags, posts }: Props) {
 }
 
 export async function getServerSideProps() {
-  const postsDatabaseId = HOME_POSTS_DATABASE_ID || '';
-
-  // parse Tags
-  const tagsDatabase = await getDatabaseForTag(postsDatabaseId);
-  const tags = (tagsDatabase.properties.tags as MultiSelectType).multi_select.options;
-
-  // parse posts
-  const postsDatabase = await getDatabaseForPosts(postsDatabaseId);
-  const posts = postsDatabase.results //
-    .map((value: any) => ({
-      id: value.id,
-      title: value.properties.title.title[0]['plain_text'],
-      tags: value.properties.tags['multi_select'],
-      description: value.properties.description['rich_text'][0]['plain_text'],
-      createdTime: new Date(value.created_time).toLocaleDateString(),
-    }));
+  const { tags, posts } = await request('/api/');
 
   return {
     props: {
