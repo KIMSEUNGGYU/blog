9c8777e6810ef18ba26fe980324e0da74e548cee
kimseunggyu <dev.seunggyu@gmail.com>
2022-06-10 21:22:28 +0900

    [chore] github actions ci 스크립트 작성

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   defaultCommandTimeout: 1000,
+  video: false,
   e2e: {
     baseUrl: 'http://localhost:3000/',
     supportFile: false,
