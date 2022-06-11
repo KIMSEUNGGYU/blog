15534ee72c173b75b2f674ceaaf82c74c131f0df
kimseunggyu <dev.seunggyu@gmail.com>
2022-06-11 22:32:11 +0900

    [refactor] PostItem 스타일 수정

--- src/components/PostItem/styles.ts
+++ src/components/PostItem/styles.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const PostItem = styled.li`
   list-style: none;
-  background-color: white;
+  background-color: ${({ theme }) => theme.background.second};
   margin-bottom: 1rem;
   border: 1px solid rgba(0, 0, 0, 0.15);
   border-radius: 5px;
@@ -27,18 +27,17 @@ export const Title = styled.h1`
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 1;
     overflow: hidden;
-    color: rgba(0, 0, 0, 0.85);
   }
 `;
 
 export const Description = styled.div`
   margin-top: 0.5rem;
+  font-weight: 500;
   & span {
     display: -webkit-box;
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 2;
     overflow: hidden;
-    color: rgba(0, 0, 0, 0.65);
   }
 `;
 
@@ -47,19 +46,4 @@ export const Footer = styled.div`
   justify-content: space-between;
 `;
 
-export const Tags = styled.ul`
-  display: flex;
-  margin-top: 0.5rem;
-  p {
-    color: #1890ff;
-  }
-
-  li {
-    margin-right: 0.6rem;
-  }
-`;
-
-export const CreatedAt = styled.p`
-  font-weight: 600;
-  color: rgba(0, 0, 0, 0.65);
-`;
+export const CreatedAt = styled.p``;
