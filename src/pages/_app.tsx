7a7d84f31e1107892f70c3ab50b0996dd5828674
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-06-11 20:50:16 +0900

    [feat] dark theme 스타일 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import type { AppProps } from 'next/app';
-
-import { Global } from '@emotion/react';
-
-import { Provider } from 'react-redux';
-
 import 'react-notion-x/src/styles.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'katex/dist/katex.min.css';
 
+import type { AppProps } from 'next/app';
+
+import { Global, ThemeProvider } from '@emotion/react';
+
+import { Provider } from 'react-redux';
+
 import store from '@/src/modules/store';
 
 import global from '@/styles/global';
+import dark from '@/styles/theme';
 
 import DefaultLayout from '../layouts';
 
@@ -19,9 +20,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Global styles={global} />
       <Provider store={store}>
-        <DefaultLayout>
-          <Component {...pageProps} />
-        </DefaultLayout>
+        <ThemeProvider theme={dark}>
+          <DefaultLayout>
+            <Component {...pageProps} />
+          </DefaultLayout>
+        </ThemeProvider>
       </Provider>
     </>
   );
---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+const dark = {
+  color: {
+    background: '#191919',
+    secondBackground: '#262626',
+    thirdBackground: '#333333',
+    brand: '#177ddc', //
+    main: '#177ddc',
+    hover: '#1866af',
+    active: '#3c9be8',
+
+    // Neutral - text
+    title: 'rgba(255, 255, 255, 85)',
+    primary: 'rgba(255, 255, 255, 85)',
+    secondary: 'rgba(255, 255, 255, 45)',
+    disable: 'rgba(255, 255, 255, 30)',
+    border: 'rgba(255, 255, 255, 20)',
+    divider: 'rgba(255, 255, 255, 12)',
+    // background: 'rgba(255, 255, 255, 8)',
+    tableHeader: 'rgba(255, 255, 255, 4)',
+  },
+};
+
+export type ThemeType = typeof dark;
+export default dark;
