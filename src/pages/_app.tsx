77b0bc3951cac0f4c5611f838f7accf841884235
kimseunggyu <dev.seunggyu@gmail.com>
2022-06-11 21:26:14 +0900

    [refactor] 전역 스타일 theme 관리 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,13 @@ import 'katex/dist/katex.min.css';
 
 import type { AppProps } from 'next/app';
 
-import { Global, ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@emotion/react';
 
 import { Provider } from 'react-redux';
 
 import store from '@/src/modules/store';
 
-import global from '@/styles/global';
+import GlobalStyle from '@/styles/globalStyle';
 import dark from '@/styles/theme';
 
 import DefaultLayout from '../layouts';
@@ -18,14 +18,14 @@ import DefaultLayout from '../layouts';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global styles={global} />
-      <Provider store={store}>
-        <ThemeProvider theme={dark}>
+      <ThemeProvider theme={dark}>
+        <GlobalStyle />
+        <Provider store={store}>
           <DefaultLayout>
             <Component {...pageProps} />
           </DefaultLayout>
-        </ThemeProvider>
-      </Provider>
+        </Provider>
+      </ThemeProvider>
     </>
   );
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,22 +1,24 @@
 const dark = {
+  background: {
+    first: '#191919',
+    second: '#262626',
+    third: '#333333',
+  },
   color: {
-    background: '#191919',
-    secondBackground: '#262626',
-    thirdBackground: '#333333',
-    brand: '#177ddc', //
+    brand: '#177ddc',
     main: '#177ddc',
     hover: '#1866af',
     active: '#3c9be8',
 
     // Neutral - text
-    title: 'rgba(255, 255, 255, 85)',
-    primary: 'rgba(255, 255, 255, 85)',
-    secondary: 'rgba(255, 255, 255, 45)',
-    disable: 'rgba(255, 255, 255, 30)',
-    border: 'rgba(255, 255, 255, 20)',
-    divider: 'rgba(255, 255, 255, 12)',
-    // background: 'rgba(255, 255, 255, 8)',
-    tableHeader: 'rgba(255, 255, 255, 4)',
+    title: 'rgba(255, 255, 255, 0.85)',
+    primary: 'rgba(255, 255, 255, 0.85)',
+    secondary: 'rgba(255, 255, 255, 0.45)',
+    disable: 'rgba(255, 255, 255, 0.30)',
+    border: 'rgba(255, 255, 255, 0.20)',
+    divider: 'rgba(255, 255, 255, 0.12)',
+    // background: 'rgba(255, 255, 255, 0.08)',
+    tableHeader: 'rgba(255, 255, 255, 0.04)',
   },
 };
 
