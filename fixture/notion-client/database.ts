93371407480abc60a1ab1ca7fb1b8df5a0b3ac3a
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-06-21 13:12:46 +0900

    [test] fixture 데이터 추가

---
+++ fixture/notion-client/database.ts
@@ -0,0 +1,93 @@
+// const DATABASE = {
+//   object: 'database',
+//   id: 'c252d515-bdd1-402f-b7a6-9ba5961eb569',
+//   cover: null,
+//   icon: null,
+//   created_time: '2022-03-12T05:53:00.000Z',
+//   created_by: { object: 'user', id: 'e0e17767-cc00-45ca-9fee-4fafb2becdd7' },
+//   last_edited_by: { object: 'user', id: 'e0e17767-cc00-45ca-9fee-4fafb2becdd7' },
+//   last_edited_time: '2022-06-19T18:24:00.000Z',
+//   title: [
+//     {
+//       type: 'text',
+//       text: [Object],
+//       annotations: [Object],
+//       plain_text: '개발용',
+//       href: null,
+//     },
+//   ],
+//   description: [],
+//   is_inline: true,
+//   properties: {
+//     description: {
+//       id: 'JNoX',
+//       name: 'description',
+//       type: 'rich_text',
+//       rich_text: {},
+//     },
+//     tags: {
+//       id: '%60cnM',
+//       name: 'tags',
+//       type: 'multi_select',
+//       multi_select: [Object],
+//     },
+//     title: { id: 'title', name: 'title', type: 'title', title: {} },
+//   },
+//   parent: { type: 'page_id', page_id: '21c1eaee-53fa-41a6-a689-93e626561ef2' },
+//   url: 'https://www.notion.so/c252d515bdd1402fb7a69ba5961eb569',
+//   archived: false,
+// };
+
+// export default DATABASE;
+
+const DATABASE = {
+  object: 'database',
+  id: 'bc1211ca-e3f1-4939-ae34-5260b16f627c',
+  created_time: '2021-07-08T23:50:00.000Z',
+  last_edited_time: '2021-07-08T23:50:00.000Z',
+  icon: {
+    type: 'emoji',
+    emoji: '🎉',
+  },
+  cover: {
+    type: 'external',
+    external: {
+      url: 'https://website.domain/images/image.png',
+    },
+  },
+  url: 'https://www.notion.so/bc1211cae3f14939ae34260b16f627c',
+  title: [
+    {
+      type: 'text',
+      text: {
+        content: 'Grocery List',
+        link: null,
+      },
+      annotations: {
+        bold: false,
+        italic: false,
+        strikethrough: false,
+        underline: false,
+        code: false,
+        color: 'default',
+      },
+      plain_text: 'Grocery List',
+      href: null,
+    },
+  ],
+  properties: {
+    tags: {
+      id: '%60cnM',
+      name: 'tags',
+      type: 'multi_select',
+      multi_select: [Object],
+    },
+  },
+  parent: {
+    type: 'page_id',
+    page_id: '98ad959b-2b6a-4774-80ee-00246fb0ea9b',
+  },
+  archived: false,
+};
+
+export default DATABASE;
---
+++ fixture/notion-client/page.ts
@@ -0,0 +1,46 @@
+const PAGE = {
+  id: 'id',
+  created_time: '2020. 3. 18.',
+  last_edited_time: '2020-03-17T21:49:37.913Z',
+  created_by: {
+    object: 'user',
+    id: 'ee5f0f84-409a-440f-983a-a5315961c6e4',
+  },
+  last_edited_by: {
+    object: 'user',
+    id: 'ee5f0f84-409a-440f-983a-a5315961c6e4',
+  },
+  parent: {
+    type: 'database_id',
+    database_id: '48f8fee9-cd79-4180-bc2f-ec0398253067',
+  },
+  archived: false,
+  url: 'https://www.notion.so/Avocado-b55c9c91384d452b81dbd1ef79372b75',
+  icon: {
+    type: 'emoji',
+    emoji: '🎉',
+  },
+  cover: {
+    type: 'external',
+    external: {
+      url: 'https://website.domain/images/image.png',
+    },
+  },
+  properties: {
+    title: {
+      title: [
+        {
+          plain_Text: '',
+        },
+      ],
+    },
+    tags: {
+      multi_select: '',
+    },
+    description: {
+      rich_text: [{ plain_text: '' }],
+    },
+  },
+};
+
+export default PAGE;
