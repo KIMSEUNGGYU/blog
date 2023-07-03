a2cf91361225700616199a10c708d558c6e6e83b
kimseunggyu <dev.seunggyu@gmail.com>
2023-07-03 23:07:56 +0900

    refactor : Code 스타일 dynamic 제거

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -3,14 +3,9 @@ import { NotionRenderer } from 'react-notion-x';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import dynamic from 'next/dynamic';
-const Code = dynamic(() =>
-  import('react-notion-x/build/third-party/code').then(async (m: any) => {
-    // additional prism syntaxes
-    await Promise.all([import('prismjs/components/prism-bash.min.js')]);
-    return m.Code;
-  }),
-);
+import('prismjs/components/prism-bash.min.js');
+import { Code } from 'react-notion-x/build/third-party/code';
+
 import { Collection } from 'react-notion-x/build/third-party/collection';
 
 import { ExtendedRecordMap } from 'notion-types';
